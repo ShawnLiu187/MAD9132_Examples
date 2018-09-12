ebbf0889bf0b15839e3ab4d5b07f39dd386f6848
Eric Torunski <etorunski@gmail.com>
2018-09-12 11:17:51 -0400

    working on layouts and views

--- app/src/main/java/com/example/etorunski/mad9132_examples/MainActivity.kt
+++ app/src/main/java/com/example/etorunski/mad9132_examples/MainActivity.kt
@@ -3,25 +3,22 @@ package com.example.etorunski.mad9132_examples
 import android.app.Activity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var aNumber = 10
 
-        if( aNumber < 20) {
-            Log.i("Output", "A number is less than 20")
-        }
-        else
-            Log.i("Output", "A number is greater than 20")
+        var textView = findViewById(R.id.firstView) as? EditText
 
-        val aList = listOf("First", "Second", "Third")
-        aList[1]
-        for(index in 0 .. (aList.size -1))
-        {
-            Log.i("Index", ""+index)
-        }
+        textView?.setOnClickListener(View.OnClickListener { Log.i("Clicked", "Clicked") })
+
+
+
+
     }
 }
