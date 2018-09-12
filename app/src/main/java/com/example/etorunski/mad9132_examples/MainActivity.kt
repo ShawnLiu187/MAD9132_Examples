6257205e06654a11e18716f4568f7ac35c6279d7
Eric Torunski <etorunski@gmail.com>
2018-09-12 11:49:26 -0400

    finished week 2 examples

--- app/src/main/java/com/example/etorunski/mad9132_examples/MainActivity.kt
+++ app/src/main/java/com/example/etorunski/mad9132_examples/MainActivity.kt
@@ -14,20 +14,21 @@ class MainActivity : Activity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var textView = findViewById(R.id.firstView) as? EditText
+        var editText = findViewById(R.id.firstView) as? EditText
 
 
         var button1 = findViewById(R.id.firstButton) as? Button
         button1?.setOnClickListener( View.OnClickListener {
-            textView?.setText("You clicked button 1")
+            editText?.setText("You clicked button 1")
 
         })
 
 
         var button2 = findViewById(R.id.secondButton) as? Button
 
+
         button2?.setOnClickListener(View.OnClickListener {
-            textView?.setText("You clicked button 2")
+            editText?.setText("You clicked button 2")
         })
     }
 }
