ff2ab1e80714d54a55dbf42c00a98db9912bcc06
Eric Torunski <etorunski@gmail.com>
2018-09-19 11:56:28 -0400

    finished week 3 examples

--- app/src/main/java/com/example/etorunski/mad9132_examples/MainActivity.kt
+++ app/src/main/java/com/example/etorunski/mad9132_examples/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.etorunski.mad9132_examples
 
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
@@ -16,13 +17,28 @@ class MainActivity : Activity() {
 
         val editText = findViewById(R.id.firstView) as? EditText
 
+        val prefs = getSharedPreferences("MyFile", Context.MODE_PRIVATE)
+
+
+        val previousString = prefs.getString("PREVIOUS_NAME", "New Name")
+        editText!!.setText(previousString)
+
 
         var button1 = findViewById(R.id.firstButton) as? Button
         button1?.setOnClickListener( View.OnClickListener {
 
+            Log.e("MainActivity", "Saved shared preferences")
+
+            var userInput = editText!!.getText().toString()
+
+            val editor = prefs.edit()
+            editor.putString("PREVIOUS_NAME", userInput)
+
+            editor.commit()
+
             val newActivity = Intent( this, InformationScreen::class.java)
 
-            newActivity.putExtra("UserInput", editText!!.getText().toString())
+            newActivity.putExtra("UserInput", userInput)
 
             startActivityForResult( newActivity , 50)
 
@@ -32,6 +48,13 @@ class MainActivity : Activity() {
 
         var button2 = findViewById(R.id.secondButton) as? Button
         button2?.setOnClickListener(View.OnClickListener {
+
+            var userInput = editText!!.getText().toString()
+
+            val editor = prefs.edit()
+            editor.putString("PREVIOUS_NAME", userInput)
+
+            editor.commit()
 
             val newActivity = Intent( this, ListScreen::class.java)
 
